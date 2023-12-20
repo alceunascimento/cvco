679d3a51cfa5d0266249029b14d3570c7a0dd7a8
AlceuN <74714670+alceunascimento@users.noreply.github.com>
2023-12-20 17:18:59 -0300

    update

---
+++ dictionary.R
@@ -0,0 +1,50 @@
+# INFORMACOES
+
+# No site da PMC
+
+## Na pagina de procura ----
+
+# parametro: Indicacao fiscla numero 32097011 sublote 000
+
+### identificação do seletor de tipo de entrada "Indicação Fiscal"
+<input id="rdoTipoPesquisa_3" type="radio" name="rdoTipoPesquisa" value="3" checked="checked">
+
+  
+# identificação do campo "Indicação Fiscal:"
+<input name="strIndFiscal" type="text" maxlength="8" id="strIndFiscal" style="width:100px;">
+
+# identificaçaõ do campo "Sublote:"
+<input name="strSublote" type="text" maxlength="4" id="strSublote" style="width:100px;">
+
+# identificação do botão de procura (lupa)
+<input type="image" name="WUCBarra1$Pesquisar" id="WUCBarra1_Pesquisar" alt="Pesquisar" src="images/botoes/normal/Pesquisar.gif" onmouseover="this.src='images/botoes/over/Pesquisar.gif'" onmouseout="this.src='images/botoes/normal/Pesquisar.gif'" onclick="return Pesquisar();;" style="border-width:0px;cursor: hand">
+
+## Na pagina da tabela ----
+
+# identificação do número do documento na tabela (coluna 01)
+<a href="javascript: Jump(281992)">281992</a>
+
+# identificação do tipo de documento  (coluna 02)
+<td align="left">&nbsp;FOLHA DO ALVARÁ</td>
+  
+# identificação do Proprietário (coluna 03)
+<td align="left">&nbsp;GLAYDSON ANGELI DONADIA</td>
+  
+# identificação do documento do proprietario (CPF/CNPJ)  (coluna 03)  
+<td align="left">&nbsp;95801405100</td>
+  
+# identificação do endreço  (coluna 04)  
+<td align="left">&nbsp;R. MAUÁ 000254 ALTO DA GLÓRIA PR  CEP:80030200</td>
+  
+# identificação do cabeçalho das colunas
+<tr class="Title1" style="height:25px;">
+  <td align="left" style="width:15%;">Número do Documento</td>
+  <td align="left" style="width:15%;">Tipo</td>
+  <td align="left" style="width:25%;">Proprietário</td>
+  <td align="left" style="width:15%;">Documento</td>
+  <td align="left" style="width:30%;">Endereço</td>
+
+  
+
+
+  
